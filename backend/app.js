af15da5eb9e279825d49f443e14b7478b269c336
shaniqbal1 <snnn757@gmail.com>
2026-04-11 18:04:18 +0500

    db start working

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,9 @@
 import express from "express";
 import  dotenv from "dotenv";
 import cors from "cors";
-
-dotenv.config();
 const app = express();
+dotenv.config();
+
 app.use(cors())
 
 //routes
--- backend/script.js
+++ backend/script.js
@@ -1,9 +1,11 @@
 import chalk from "chalk";
-import ConnectDB from "./config/db.config";
+import express from "express"
+import ConnectDB from "./config/db.config.js";
+const app = express();
 const PORT = process.env.PORT || 7000;``
 ConnectDB()
 .then(()=>{
-    app.listen(PiOrangeThin,()=>{
+    app.listen(PORT,()=>{
         console.log(chalk.green.bgBlack.bold.underline("server is running on port ",PORT));
         
     })
